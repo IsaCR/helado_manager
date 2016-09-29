a9217786fedcefb4747443d5e89567ef85461e28
Isabel <isagonzacr@gmail.com>
2016-09-29 11:52:33 -0600

    Add User model, controller and views with devise
    Commands run:
     $ rails generate devise User
     $ rake db:migrate
     $ rails generate devise:views
     $ rails generate devise:controllers users
    
    * Run migrations when downloading this commit
    * ToDo: configure routes, database.yml
            modify views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
+  # root to: "home#index"
+
   devise_for :installs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions'
+             },
+             path: 'auth',
+             path_names: {
+               sign_in: 'login',
+               sign_out: 'logout',
+               password: 'secret',
+               confirmation: 'verification',
+               unlock: 'unblock',
+               registration: 'register',
+               sign_up: 'cmon_let_me_in'
+             }
 end
-root to: "home#index"
